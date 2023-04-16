af1478bf0b5924397d00b3e810a646625e92e1ca
Neylane <neylanepl@gmail.com>
2023-04-16 11:39:37 -0300

    feat: main inicial de cliente e servidor

--- prog-distribuida/src/MainClient.java
+++ prog-distribuida/src/MainClient.java
@@ -1,5 +1,16 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
 public class MainClient {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
+
+		ServerInterface server = (ServerInterface) 
+				Naming.lookup("rmi://127.0.0.1:1098/ServerCallbak");
+
+		ClientInterface client = new Client();
+		
+		server.registerClient(client);
+	}
 }
--- prog-distribuida/src/MainServer.java
+++ prog-distribuida/src/MainServer.java
@@ -1,5 +1,20 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
 public class MainServer {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+    public static void main(String[] args) throws RemoteException, MalformedURLException {
+		
+		System.setProperty("java.rmi.server.hostname","127.0.0.1");
+		
+		ServerInterface server = new Server();
+
+		LocateRegistry.createRegistry(1098);
+		
+		Naming.rebind("rmi://127.0.0.1:1098/ServerCallbak", server);
+		
+		System.out.println("RMI Callback Server Starterd.");
+		
+	}
 }
